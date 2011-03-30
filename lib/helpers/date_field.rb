73a4724f54236c848918b4711f02687b5757aa09
Blade Yu <yakjuly@gmail.com>
2011-03-30 11:05:22 +0800

    change webcalendar back

--- lib/helpers/date_field.rb
+++ lib/helpers/date_field.rb
@@ -3,6 +3,48 @@ module ActionView
     module FormHelper
       def date_field(object_name, method, options = {})
         obj = ::ActionView::Helpers::InstanceTag.new(object_name, method, self).object
+        format = (options[:only_date] ? :date : :long)
+        text_field(object_name, method, options.merge(:value => ((v=obj.try(method)).blank? ? options[:value] : v.try(:to_s,format)), :onclick=>date_onclick(options)))
+      end
+
+      def date_field_tag(name, value =nil, options= {})
+        text_field_tag(name, value, options.merge(:onclick=>date_onclick(options)))
+      end
+
+      def rich_text_area(object_name, method, options = {})
+        rich_editor_files +
+          text_area(object_name, method, options.merge({:id => "fancypants"}))
+      end
+      
+      def rich_text_area_tag(name, content = nil, options = {})
+        rich_editor_files +
+          text_area_tag(name, content, options.merge({:id => "fancypants"}))
+      end
+
+      private
+      def date_onclick(options={})
+        "SelectDate(this,'yyyy-MM-dd',#{I18n.locale.to_s=="zh" ? 0 : 1}, #{options[:only_date] ? 0 : 1}, #{options[:begin]||1}, #{options[:end]||1})"
+      end
+    end
+
+    class FormBuilder #:nodoc:
+      def date_field(method, options = {})
+        @template.date_field(@object_name, method, objectify_options(options))
+      end
+
+      def rich_text_area(method, options = {})
+        @template.rich_text_area(@object_name, method, objectify_options(options))
+      end
+    end
+  end
+end
+
+=begin
+module ActionView
+  module Helpers
+    module FormHelper
+      def date_field(object_name, method, options = {})
+        obj = ::ActionView::Helpers::InstanceTag.new(object_name, method, self).object
         date_picker_options = options.extract!(:change, :format, :range)
         value = ((v=obj.try(method)).blank? ? options[:value] : v.try(:to_s,:date))
 
@@ -46,3 +88,4 @@ module ActionView
     end
   end
 end
+=end
